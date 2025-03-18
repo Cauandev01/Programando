1cecc2f7ba6cffdc06d2828d428e8848a08419f9
Cauandev01 <cauanjorge76@gmail.com>
2025-03-18 16:08:01 -0300

    Update trunfo.c

--- supertrunfo/trunfo.c
+++ supertrunfo/trunfo.c
@@ -1,45 +1,95 @@
 #include <stdio.h>
 
 int main() {
+    // Declaração das variáveis para dois países
+    char nome1[20], nome2[20];
+    int populacao1, populacao2, pontos1, pontos2;
+    float area1, area2, pib1, pib2;
+    float densidade1, densidade2, pibper1, pibper2, superPoder1, superPoder2;
+    int escolha;
 
-char nome[20];
-    int populacao;
-    float area;
-    float pib;
-    int pontos;
-    float densidade;
-    float pibper;
-    printf("=== Bem vindo ao super trunfo - paises ===\n\n");
-    printf("No jogo Super Trunfo, os jogadores comparam as propriedades das cartas para determinar a mais forte.\n\n");
-   
+    printf("=== Bem vindo ao Super Trunfo - Paises ===\n\n");
+
     
-   
-    //Solicitando nome da cidade
-    printf("Digite o nome da cidade:\n");
-    scanf("%s" , nome);
-    //Solicitando o restante dos atributos
+    printf("Digite o nome da primeira cidade:\n");
+    scanf("%s", nome1);
     printf("População:\n");
-    scanf("%d" , &populacao);
+    scanf("%d", &populacao1);
     printf("Área (em km²):\n");
-    scanf("%f" , &area);
-    printf("Pib (em milhões):\n");
-    scanf("%f" , &pib);
-    printf("Números de pontos turisticos:\n");
-    scanf("%d" , &pontos);
-   
+    scanf("%f", &area1);
+    printf("PIB (em milhões):\n");
+    scanf("%f", &pib1);
+    printf("Número de pontos turísticos:\n");
+    scanf("%d", &pontos1);
 
-    densidade = populacao / area; 
+    // Captura de dados para o segundo país
+    printf("\nDigite o nome da segunda cidade:\n");
+    scanf("%s", nome2);
+    printf("População:\n");
+    scanf("%d", &populacao2);
+    printf("Área (em km²):\n");
+    scanf("%f", &area2);
+    printf("PIB (em milhões):\n");
+    scanf("%f", &pib2);
+    printf("Número de pontos turísticos:\n");
+    scanf("%d", &pontos2);
 
-    pibper = (pib * 1000000) / populacao;
+    // Cálculo dos atributos
+    densidade1 = populacao1 / area1;
+    pibper1 = (pib1 * 1000000) / populacao1;
+    superPoder1 = populacao1 + pibper1 + pontos1 - densidade1;
 
-// saida de dados
+    densidade2 = populacao2 / area2;
+    pibper2 = (pib2 * 1000000) / populacao2;
+    superPoder2 = populacao2 + pibper2 + pontos2 - densidade2;
 
-    printf("Nome: %s\n" , nome);
-    printf("População: %d\n" , populacao);
-    printf("Área: %.2f\n" , area);
-    printf("Pib: %.2f\n" , pib);
-    printf("Pontos turistico: %d\n" , pontos);
-    printf("Densidade populacional: %.3f\n" , densidade);
-    printf("Pib per capita %.3f\n", pibper);
+    // Exibição dos dados
+    printf("\n=== Dados dos países ===\n");
+    printf("%s -> População: %d, Densidade: %.2f, PIB per capita: %.2f, Pontos turísticos: %d, Super Poder: %.2f\n",
+           nome1, populacao1, densidade1, pibper1, pontos1, superPoder1);
+    printf("%s -> População: %d, Densidade: %.2f, PIB per capita: %.2f, Pontos turísticos: %d, Super Poder: %.2f\n",
+           nome2, populacao2, densidade2, pibper2, pontos2, superPoder2);
+
+    // Escolha do atributo para comparar
+    printf("\nEscolha um atributo para comparar:\n");
+    printf("1 - População\n2 - Densidade Populacional (menor vence)\n3 - PIB per capita\n4 - Pontos Turísticos\n5 - Super Poder\n");
+    scanf("%d", &escolha);
+
+    // Lógica de comparação
+    switch (escolha) {
+        case 1:
+            if (populacao1 > populacao2)
+                printf("Vencedor: %s\n", nome1);
+            else
+                printf("Vencedor: %s\n", nome2);
+            break;
+        case 2:
+            if (densidade1 < densidade2)
+                printf("Vencedor: %s\n", nome1);
+            else
+                printf("Vencedor: %s\n", nome2);
+            break;
+        case 3:
+            if (pibper1 > pibper2)
+                printf("Vencedor: %s\n", nome1);
+            else
+                printf("Vencedor: %s\n", nome2);
+            break;
+        case 4:
+            if (pontos1 > pontos2)
+                printf("Vencedor: %s\n", nome1);
+            else
+                printf("Vencedor: %s\n", nome2);
+            break;
+        case 5:
+            if (superPoder1 > superPoder2)
+                printf("Vencedor: %s\n", nome1);
+            else
+                printf("Vencedor: %s\n", nome2);
+            break;
+        default:
+            printf("Opção inválida.\n");
+    }
+
     return 0;
 }
