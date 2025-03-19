a552b27d030182e82b2bc3a75771307300b67296
Cauandev01 <cauanjorge76@gmail.com>
2025-03-19 16:14:13 -0300

    Update trunfo.c

--- supertrunfo/trunfo.c
+++ supertrunfo/trunfo.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    // Declaração das variáveis para dois países
-    char nome1[20], nome2[20];
+    char nome1[30], nome2[30]; // Nome das cidades
     int populacao1, populacao2, pontos1, pontos2;
     float area1, area2, pib1, pib2;
     float densidade1, densidade2, pibper1, pibper2, superPoder1, superPoder2;
     int escolha;
 
-    printf("=== Bem vindo ao Super Trunfo - Paises ===\n\n");
+    printf("=== Bem-vindo ao Super Trunfo - Países ===\n\n");
 
-    
+   
     printf("Digite o nome da primeira cidade:\n");
-    scanf("%s", nome1);
+    scanf(" %[^\n]", nome1);
     printf("População:\n");
     scanf("%d", &populacao1);
     printf("Área (em km²):\n");
@@ -22,9 +21,9 @@ int main() {
     printf("Número de pontos turísticos:\n");
     scanf("%d", &pontos1);
 
-    // Captura de dados para o segundo país
+  
     printf("\nDigite o nome da segunda cidade:\n");
-    scanf("%s", nome2);
+    scanf(" %[^\n]", nome2);
     printf("População:\n");
     scanf("%d", &populacao2);
     printf("Área (em km²):\n");
@@ -34,16 +33,31 @@ int main() {
     printf("Número de pontos turísticos:\n");
     scanf("%d", &pontos2);
 
-    // Cálculo dos atributos
-    densidade1 = populacao1 / area1;
-    pibper1 = (pib1 * 1000000) / populacao1;
+    if (area1 != 0)
+        densidade1 = populacao1 / area1;
+    else
+        densidade1 = 0;
+
+    if (populacao1 != 0)
+        pibper1 = pib1 / populacao1;
+    else
+        pibper1 = 0;
+
     superPoder1 = populacao1 + pibper1 + pontos1 - densidade1;
 
-    densidade2 = populacao2 / area2;
-    pibper2 = (pib2 * 1000000) / populacao2;
+    if (area2 != 0)
+        densidade2 = populacao2 / area2;
+    else
+        densidade2 = 0;
+
+    if (populacao2 != 0)
+        pibper2 = pib2 / populacao2;
+    else
+        pibper2 = 0;
+
     superPoder2 = populacao2 + pibper2 + pontos2 - densidade2;
 
-    // Exibição dos dados
+  
     printf("\n=== Dados dos países ===\n");
     printf("%s -> População: %d, Densidade: %.2f, PIB per capita: %.2f, Pontos turísticos: %d, Super Poder: %.2f\n",
            nome1, populacao1, densidade1, pibper1, pontos1, superPoder1);
@@ -55,41 +69,46 @@ int main() {
     printf("1 - População\n2 - Densidade Populacional (menor vence)\n3 - PIB per capita\n4 - Pontos Turísticos\n5 - Super Poder\n");
     scanf("%d", &escolha);
 
-    // Lógica de comparação
-    switch (escolha) {
-        case 1:
-            if (populacao1 > populacao2)
-                printf("Vencedor: %s\n", nome1);
-            else
-                printf("Vencedor: %s\n", nome2);
-            break;
-        case 2:
-            if (densidade1 < densidade2)
-                printf("Vencedor: %s\n", nome1);
-            else
-                printf("Vencedor: %s\n", nome2);
-            break;
-        case 3:
-            if (pibper1 > pibper2)
-                printf("Vencedor: %s\n", nome1);
-            else
-                printf("Vencedor: %s\n", nome2);
-            break;
-        case 4:
-            if (pontos1 > pontos2)
-                printf("Vencedor: %s\n", nome1);
-            else
-                printf("Vencedor: %s\n", nome2);
-            break;
-        case 5:
-            if (superPoder1 > superPoder2)
-                printf("Vencedor: %s\n", nome1);
-            else
-                printf("Vencedor: %s\n", nome2);
-            break;
-        default:
-            printf("Opção inválida.\n");
+    
+    if (escolha == 1) {
+        if (populacao1 > populacao2)
+            printf("Vencedor: %s\n", nome1);
+        else if (populacao2 > populacao1)
+            printf("Vencedor: %s\n", nome2);
+        else
+            printf("Empate!\n");
+    } else if (escolha == 2) {
+        if (densidade1 < densidade2)
+            printf("Vencedor: %s\n", nome1);
+        else if (densidade2 < densidade1)
+            printf("Vencedor: %s\n", nome2);
+        else
+            printf("Empate!\n");
+    } else if (escolha == 3) {
+        if (pibper1 > pibper2)
+            printf("Vencedor: %s\n", nome1);
+        else if (pibper2 > pibper1)
+            printf("Vencedor: %s\n", nome2);
+        else
+            printf("Empate!\n");
+    } else if (escolha == 4) {
+        if (pontos1 > pontos2)
+            printf("Vencedor: %s\n", nome1);
+        else if (pontos2 > pontos1)
+            printf("Vencedor: %s\n", nome2);
+        else
+            printf("Empate!\n");
+    } else if (escolha == 5) {
+        if (superPoder1 > superPoder2)
+            printf("Vencedor: %s\n", nome1);
+        else if (superPoder2 > superPoder1)
+            printf("Vencedor: %s\n", nome2);
+        else
+            printf("Empate!\n");
+    } else {
+        printf("Opção inválida.\n");
     }
 
     return 0;
 }
+
